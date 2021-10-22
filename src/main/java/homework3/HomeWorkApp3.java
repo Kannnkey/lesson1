56871d7035efb9043a6a42826d9abb6ef9bb6580
Kannnkey <bahen27@mail.ru>
2021-10-22 16:13:43 +0300

    home task 3 done

--- src/main/java/homework3/HomeWorkApp3.java
+++ src/main/java/homework3/HomeWorkApp3.java
@@ -6,7 +6,7 @@ public class HomeWorkApp3 {
     public static void main(String[] args) {
         invertArray();
         fillArray();
-
+        changeArray();
     }
 
     public static void invertArray() {
@@ -31,6 +31,13 @@ public class HomeWorkApp3 {
     }
 
 
-
+        public static void changeArray() {
+            int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;}
+                System.out.print(arr[i] + " ");
+            }System.out.println();
+        }
 
 }
