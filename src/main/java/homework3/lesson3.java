bbb94238cad3404f638e5f37e97e44a070132937
Kannnkey <bahen27@mail.ru>
2021-10-24 17:17:54 +0300

    home task 5 done

--- src/main/java/homework3/lesson3.java
+++ src/main/java/homework3/lesson3.java
@@ -4,14 +4,13 @@ import java.util.Random;
 
 public class lesson3 {
     public static void main(String[] args) {
-        int[] b = new int[10];
+        int [][] a = {
+                {1, 0, 0, 0, 1},
+                {0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 0,},
+                {0, 1, 0, 1, 0},
+                {1, 0, 0, 0, 1}
 
-        final Random random = new Random();
-        for (int i = 0; i < b.length; i++) {
-            b[i] = random.nextInt(100);
-        }
-        for (int i = 0; i < b.length; i++) {
-        System.out.println("b[" + i + "] = " + b[i]);
-        }
+        };
     }
 }
